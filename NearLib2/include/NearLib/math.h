44fa5f713b1934df251fda043ca940adc9513ec2
i-yuuki <i-yuuki@users.noreply.github.com>
2022-07-30 23:31:04 +0900

    円周率とラジアン変換をmath.hに追加

--- NearLib2/include/NearLib/math.h
+++ NearLib2/include/NearLib/math.h
@@ -7,4 +7,14 @@ namespace Near::Math{
 // MARUNAGE
 using namespace DirectX::SimpleMath;
 
+constexpr float PI = DirectX::XM_PI;
+
+constexpr float toRadians(float degrees){
+  return DirectX::XMConvertToRadians(degrees);
+}
+
+constexpr float toDegrees(float radians){
+  return DirectX::XMConvertToDegrees(radians);
+}
+
 }
