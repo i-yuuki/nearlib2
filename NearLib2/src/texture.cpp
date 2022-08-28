3324808de0302adebe3477058ba21bc4ddcb822a
i-yuuki <i-yuuki@users.noreply.github.com>
2022-08-28 23:23:51 +0900

    テクスチャ読み込みでsRGB変換しない
    NearLib1に合わせる＆作ったテクスチャ何やっても暗くなるの腹立つ

--- NearLib2/src/texture.cpp
+++ NearLib2/src/texture.cpp
@@ -9,7 +9,7 @@
 namespace Near{
 
 void Texture::load(const std::string& path){
-  HRESULT res = DirectX::CreateWICTextureFromFileEx(window.getDevice(), widen(path).c_str(), 0, D3D11_USAGE_DEFAULT, D3D11_BIND_SHADER_RESOURCE, 0, 0, DirectX::WIC_LOADER_DEFAULT, nullptr, &textureView);
+  HRESULT res = DirectX::CreateWICTextureFromFileEx(window.getDevice(), widen(path).c_str(), 0, D3D11_USAGE_DEFAULT, D3D11_BIND_SHADER_RESOURCE, 0, 0, DirectX::WIC_LOADER_IGNORE_SRGB, nullptr, &textureView);
   if(FAILED(res)) throwResult(std::string("Failed to load texture from file ").append(path).c_str(), res);
 }
 
