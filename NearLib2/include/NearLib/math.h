8bf22df5c11c42ca14fa19314129180593129f9d
i-yuuki <i-yuuki@users.noreply.github.com>
2022-09-02 15:36:40 +0900

    数値リマップ君 (Math::map) を追加

--- NearLib2/include/NearLib/math.h
+++ NearLib2/include/NearLib/math.h
@@ -17,4 +17,16 @@ constexpr float toDegrees(float radians){
   return DirectX::XMConvertToDegrees(radians);
 }
 
+// 数値をある範囲から別の範囲にリマップします。
+// たとえば valueStart～valueEnd の真ん中の値を渡すと
+// targetStart～targetEnd の真ん中の値が返ります。
+// @param clamp `value`を`valueStart`～`valueEnd`の範囲に収めてからリマップするかどうか
+template<typename T>
+constexpr T map(T value, T valueStart, T valueEnd, T targetStart, T targetEnd, bool clamp = false){
+  if(clamp){
+    value = std::clamp(value, std::min(valueStart, valueEnd), std::max(valueStart, valueEnd));
+  }
+  return targetStart + ((value - valueStart) / (valueEnd - valueStart)) * (targetEnd - targetStart);
+}
+
 }
